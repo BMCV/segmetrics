0c911dc889be9f4909ab209d60b1aa7e24c87d5f
Leonid Kostrykin <void@evoid.de>
2018-07-04 19:49:23 +0200

    fixes bug in `__init__.py`

--- segmetrics/__init__.py
+++ segmetrics/__init__.py
@@ -3,11 +3,12 @@ import regional
 import detection
 import study
 import legacy
+import parallel
 
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 10
-VERSION_PATCH = 10
+VERSION_PATCH = 11
 
 VERSION = '%d.%d%s' % (VERSION_MAJOR, VERSION_MINOR, '.%d' % VERSION_PATCH if VERSION_PATCH > 0 else '')
 
