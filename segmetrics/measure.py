9b4298b52aa58ffe23ca4cb8e43bef6c588223bd
Leonid Kostrykin <void@evoid.de>
2022-10-10 18:56:18 +0200

    Update measure.py

--- segmetrics/measure.py
+++ segmetrics/measure.py
@@ -1,8 +1,17 @@
 class Measure:
+    """Defines a performance measure.
+    """
 
+    """Indicates whether the results from this measure should be presented as percentages."""
     FRACTIONAL   = False
+    
+    """Indicates whether the results from this measure are aggregated by summing (``True``) or by taking the average (``False``)."""
     ACCUMULATIVE = False
 
     def set_expected(self, expected):
+        """Sets the expected result for evaluation.
+        
+        :param expected: An image containing uniquely labeled object masks corresponding to the ground truth.
+        """
         self.expected = expected
 
