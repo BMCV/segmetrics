2cd14e7d51aa6eb3ad1ccd04daded6adb8c64640
Leonid Kostrykin <void@evoid.de>
2023-04-06 13:57:40 +0200

    increments version to 1.3

--- segmetrics/__init__.py
+++ segmetrics/__init__.py
@@ -7,8 +7,8 @@ import segmetrics.parallel
 
 
 VERSION_MAJOR = 1
-VERSION_MINOR = 2
-VERSION_PATCH = 3
+VERSION_MINOR = 3
+VERSION_PATCH = 0
 
 VERSION = '%d.%d%s' % (VERSION_MAJOR, VERSION_MINOR, '.%d' % VERSION_PATCH if VERSION_PATCH > 0 else '')
 
