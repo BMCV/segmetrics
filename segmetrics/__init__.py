03b7b5c2b7b464b1c7f5afe4f3f28ac355df3657
Leonid Kostrykin <void@evoid.de>
2024-03-03 09:34:44 +0000

    Fix linting issues

--- segmetrics/__init__.py
+++ segmetrics/__init__.py
@@ -1,7 +1,24 @@
-from segmetrics.contour import Hausdorff, NSD
-from segmetrics.regional import Dice, JaccardCoefficient, JaccardIndex, RandIndex, AdjustedRandIndex, ISBIScore
-from segmetrics.detection import FalsePositive, FalseNegative, FalseSplit, FalseMerge
-from segmetrics.study import Study
+from segmetrics.contour import (
+    Hausdorff,
+    NSD,
+)
+from segmetrics.regional import (
+    Dice,
+    JaccardCoefficient,
+    JaccardIndex,
+    RandIndex,
+    AdjustedRandIndex,
+    ISBIScore,
+)
+from segmetrics.detection import (
+    FalsePositive,
+    FalseNegative,
+    FalseSplit,
+    FalseMerge,
+)
+from segmetrics.study import (
+    Study,
+)
 
 import segmetrics.parallel
 
@@ -10,4 +27,8 @@ VERSION_MAJOR = 1
 VERSION_MINOR = 5
 VERSION_PATCH = 0
 
-VERSION = '%d.%d%s' % (VERSION_MAJOR, VERSION_MINOR, '.%d' % VERSION_PATCH if VERSION_PATCH > 0 else '')
+VERSION = '%d.%d%s' % (
+    VERSION_MAJOR,
+    VERSION_MINOR,
+    '.%d' % VERSION_PATCH if VERSION_PATCH > 0 else '',
+)
