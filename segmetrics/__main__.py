e08d856e509ed54726ceae06c3c9a0ff22500a5e
Leonid Kostrykin <void@evoid.de>
2026-03-27 14:57:46 +0100

    Add `if __name__ == '__main__'`

--- segmetrics/__main__.py
+++ segmetrics/__main__.py
@@ -12,66 +12,6 @@ from . import (
 )
 from .measure import Measure
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    'seg_dir',
-    help='directory containing the segmentation results',
-)
-parser.add_argument(
-    'seg_file',
-    help='regex pattern used to parse segmentation results',
-)
-parser.add_argument(
-    'gt_file',
-    help=(
-        'pattern used to obtain the corresponding ground truth file'
-        ' (\\1 corresponds to the first capture group)'
-    ),
-)
-parser.add_argument(
-    'output_file',
-    help='filepath where the results are to be written (CSV)',
-)
-parser.add_argument(
-    '--recursive',
-    '-r',
-    action='store_true',
-    help='reads the segmentation results directory recursively',
-)
-parser.add_argument(
-    '--gt-unique',
-    action='store_true',
-    help='assumes that the ground truth data is uniquely labeled',
-)
-parser.add_argument(
-    '--seg-unique',
-    action='store_true',
-    help='assumes that the segmentation result data is uniquely labeled',
-)
-parser.add_argument(
-    'measures',
-    nargs='+',
-    type=str,
-    help='list of performance measures',
-)
-parser.add_argument(
-    '--semicolon',
-    action='store_true',
-    help='uses semi-colon instead of comma to write the results',
-)
-args = parser.parse_args()
-
-print(f'')
-print(f'Summary')
-print(f'*******')
-print(f'')
-print(f' Reading segmentation results from: {args.seg_dir}')
-print(f' Pattern used for parsing file name: {args.seg_file}')
-print(f' Pattern used for ground truth files: {args.gt_file}')
-print(f' Is ground truth data uniquely labeled? {args.gt_unique}')
-print(f' Is segmentation result data uniquely labeled? {args.seg_unique}')
-print(f' Results will be written to: {args.output_file}')
-print(f' The following performance measures will be used:')
 
 # Build dictionary of measures
 measures_dict = dict()
@@ -80,48 +20,111 @@ for measure_name in dir(measures):
     if inspect.isclass(measure) and issubclass(measure, Measure):
         measures_dict[measure_name] = measure
 
-# Build study
-study = Study()
-for measure_spec in args.measures:
-    print(f' - {measure_spec}')
-    measure = eval(measure_spec, dict(), measures_dict)
-    study.add_measure(measure)
+if __name__ == '__main__':
 
-seg_file_pattern = re.compile(args.seg_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'seg_dir',
+        help='directory containing the segmentation results',
+    )
+    parser.add_argument(
+        'seg_file',
+        help='regex pattern used to parse segmentation results',
+    )
+    parser.add_argument(
+        'gt_file',
+        help=(
+            'pattern used to obtain the corresponding ground truth file'
+            ' (\\1 corresponds to the first capture group)'
+        ),
+    )
+    parser.add_argument(
+        'output_file',
+        help='filepath where the results are to be written (CSV)',
+    )
+    parser.add_argument(
+        '--recursive',
+        '-r',
+        action='store_true',
+        help='reads the segmentation results directory recursively',
+    )
+    parser.add_argument(
+        '--gt-unique',
+        action='store_true',
+        help='assumes that the ground truth data is uniquely labeled',
+    )
+    parser.add_argument(
+        '--seg-unique',
+        action='store_true',
+        help='assumes that the segmentation result data is uniquely labeled',
+    )
+    parser.add_argument(
+        'measures',
+        nargs='+',
+        type=str,
+        help='list of performance measures',
+    )
+    parser.add_argument(
+        '--semicolon',
+        action='store_true',
+        help='uses semi-colon instead of comma to write the results',
+    )
+    args = parser.parse_args()
 
-print(f'')
-print(f'Evaluation')
-print(f'**********')
-print(f'')
+    print(f'')
+    print(f'Summary')
+    print(f'*******')
+    print(f'')
+    print(f' Reading segmentation results from: {args.seg_dir}')
+    print(f' Pattern used for parsing file name: {args.seg_file}')
+    print(f' Pattern used for ground truth files: {args.gt_file}')
+    print(f' Is ground truth data uniquely labeled? {args.gt_unique}')
+    print(f' Is segmentation result data uniquely labeled? {args.seg_unique}')
+    print(f' Results will be written to: {args.output_file}')
+    print(f' The following performance measures will be used:')
 
-glob_pattern = (
-    args.seg_dir + '**' if args.seg_dir.endswith('/')
-    else args.seg_dir + '/**'
-)
-for filepath in glob.glob(glob_pattern, recursive=args.recursive):
-    match = seg_file_pattern.match(filepath)
-    if match is None:
-        continue
-    gt_file = args.gt_file
-    for group_idx in range(len(match.groups()) + 1):
-        gt_file = gt_file.replace(
-            rf'\{group_idx:d}',
-            match.group(group_idx),
-        )
+    # Build study
+    study = Study()
+    for measure_spec in args.measures:
+        print(f' - {measure_spec}')
+        measure = eval(measure_spec, dict(), measures_dict)
+        study.add_measure(measure)
 
-    print(f'Evaluating {filepath} using ground truth: {gt_file}')
+    seg_file_pattern = re.compile(args.seg_file)
 
-    sample_id = str(pathlib.Path(filepath).relative_to(args.seg_dir))
+    print(f'')
+    print(f'Evaluation')
+    print(f'**********')
+    print(f'')
 
-    im_actual   = skimage.io.imread(filepath)
-    im_expected = skimage.io.imread(gt_file)
+    glob_pattern = (
+        args.seg_dir + '**' if args.seg_dir.endswith('/')
+        else args.seg_dir + '/**'
+    )
+    for filepath in glob.glob(glob_pattern, recursive=args.recursive):
+        match = seg_file_pattern.match(filepath)
+        if match is None:
+            continue
+        gt_file = args.gt_file
+        for group_idx in range(len(match.groups()) + 1):
+            gt_file = gt_file.replace(
+                rf'\{group_idx:d}',
+                match.group(group_idx),
+            )
 
-    study.set_expected(im_expected, unique=args.gt_unique)
-    study.process(sample_id, im_actual, unique=args.seg_unique)
+        print(f'Evaluating {filepath} using ground truth: {gt_file}')
 
-csv_delimiter = ';' if args.semicolon else ','
-with open(args.output_file, 'w') as fout:
-    study.write_csv(fout, delimiter=csv_delimiter)
+        sample_id = str(pathlib.Path(filepath).relative_to(args.seg_dir))
 
-print(f'')
-print(f'Results written to: {args.output_file}')
+        im_actual   = skimage.io.imread(filepath)
+        im_expected = skimage.io.imread(gt_file)
+
+        study.set_expected(im_expected, unique=args.gt_unique)
+        study.process(sample_id, im_actual, unique=args.seg_unique)
+
+    csv_delimiter = ';' if args.semicolon else ','
+    with open(args.output_file, 'w') as fout:
+        study.write_csv(fout, delimiter=csv_delimiter)
+
+    print(f'')
+    print(f'Results written to: {args.output_file}')
